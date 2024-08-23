63b0086dfcb71498da25d2d43a4b51a2c70d2078
Abdullah Saeed <85762594+AbdullahSaeed1211@users.noreply.github.com>
2024-08-23 21:49:49 +0530

    error fix

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -3,7 +3,7 @@ import { stripe } from "@/app/utils/stripe";
 import { headers } from "next/headers";
 import Stripe from "stripe";
 
-export async function POST(req: Request) {
+export default async function POST(req: Request) {
     const body = await req.text();
 
     const signature = headers().get('Stripe-Signature') as string;
